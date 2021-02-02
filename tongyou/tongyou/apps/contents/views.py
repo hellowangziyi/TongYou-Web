0c7a93a2a07f8cdedc34d13d119a7d41d5d00250
wzy <1164624362@qq.com>
2021-02-03 01:45:57 +0800

    展示首页商品频道分类

--- tongyou/tongyou/apps/contents/views.py
+++ tongyou/tongyou/apps/contents/views.py
@@ -3,8 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import View
 from django.shortcuts import render
-
+from goods.models import GoodsChannel, GoodsCategory
+from .utils import get_categories
 
 class IndexView(View):
     def get(self, request):
-        return render(request, 'index.html')
+        """提供首页广告界面"""
+        # 查询商品频道和分类
+        categories = get_categories()
+        # 渲染模板的上下文
+        context = {
+            'categories': categories,
+        }
+        return render(request, 'index.html', context)
