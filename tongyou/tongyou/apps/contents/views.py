526d2649f194d32f99bc7e6d80731db211c74eb5
wzy <1164624362@qq.com>
2021-02-04 01:57:57 +0800

    展示首页商品广告

--- tongyou/tongyou/apps/contents/views.py
+++ tongyou/tongyou/apps/contents/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.views import View
 from django.shortcuts import render
 from goods.models import GoodsChannel, GoodsCategory
+from .models import ContentCategory
 from .utils import get_categories
 
 class IndexView(View):
@@ -11,8 +12,15 @@ class IndexView(View):
         """提供首页广告界面"""
         # 查询商品频道和分类
         categories = get_categories()
+
+        # 广告数据
+        contents = {}
+        content_categories = ContentCategory.objects.all()
+        for cat in content_categories:
+            contents[cat.key] = cat.content_set.filter(status=True).order_by('sequence')
         # 渲染模板的上下文
         context = {
             'categories': categories,
+            'contents': contents
         }
         return render(request, 'index.html', context)
