58c03ba22771bed72dec11ce60cdabbbb54b0062
wzy <1164624362@qq.com>
2021-02-05 15:26:09 +0800

    完成商品详情页

--- tongyou/tongyou/apps/goods/urls.py
+++ tongyou/tongyou/apps/goods/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
+    # 商品列表页
     url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', views.ListView.as_view()),
-    url(r'^hot/(?P<category_id>\d+)/$', views.HotGoodsView.as_view())
+    # 热销排行
+    url(r'^hot/(?P<category_id>\d+)/$', views.HotGoodsView.as_view()),
+    # 商品详情页
+    url(r'^detail/(?P<sku_id>\d+)/$', views.DetailView.as_view()),
 ]
--- tongyou/tongyou/apps/goods/views.py
+++ tongyou/tongyou/apps/goods/views.py
@@ -3,7 +3,7 @@ from django.views import View
 from django.shortcuts import render
 from django import http
 
-from .models import GoodsChannel, GoodsCategory, SKU
+from .models import GoodsChannel, GoodsCategory, SKU, SPU
 from contents.utils import get_categories
 from .utlis import get_breadcrumb
 from tongyou.utils.response_code import RETCODE
@@ -86,3 +86,68 @@ class HotGoodsView(View):
 
 
         return http.JsonResponse({'code':RETCODE.OK,'errmsg':'OK', 'hot_skus':hot_skus})
+
+class DetailView(View):
+    """商品详情页"""
+
+    def get(self, request, sku_id):
+        # 获取当前sku的信息
+        try:
+            sku = SKU.objects.get(id=sku_id)
+        except Exception as e:
+            return http.HttpResponseForbidden("sku_id 不存在")
+
+        # 获取当前sku所对应的三级分类
+        category = sku.category
+
+        # 查询当前sku所对应的spu
+        spu = sku.spu
+
+        """1.准备当前商品的规格选项列表 [8, 11]"""
+        # 获取出当前正显示的sku商品的规格选项id列表
+        sku_spec_qs = sku.specs.order_by('spec_id')
+        sku_option_ids = []
+        for sku_spec in sku_spec_qs:
+            sku_option_ids.append(sku_spec.option_id)
+
+        """2.构造规格选择仓库
+                {(8, 11): 3, (8, 12): 4, (9, 11): 5, (9, 12): 6, (10, 11): 7, (10, 12): 8}
+        """
+        # 构造规格选择仓库
+        temp_sku_qs = spu.sku_set.all()   # 获取当前spu下的所有sku
+        spec_sku_map = {}
+        for temp_sku in temp_sku_qs:
+            # 查询每一个sku的规格数据
+            temp_spec_qs = temp_sku.specs.order_by('spec_id')
+            temp_sku_option_ids = []   # 用来包装每个sku的选项值
+            for temp_spec in temp_spec_qs:
+                temp_sku_option_ids.append(temp_spec.option_id)
+            spec_sku_map[tuple(temp_sku_option_ids)] = temp_sku.id
+
+        """3.组合 并找到sku_id 绑定"""
+        spu_spec_qs =spu.specs.order_by('id')  # 获取当前spu中的所有规格
+        for index, spec in enumerate(spu_spec_qs):
+            spec_option_qs = spec.options.all()
+            temp_option_ids = sku_option_ids[:]
+            for option in spec_option_qs:
+                temp_option_ids[index] = option.id
+                option.sku_id = spec_sku_map.get(tuple(temp_option_ids))
+            spec.spec_options = spec_option_qs
+
+
+        # 查询商品频道分类
+        categories = get_categories()
+        # 查询面包屑导航
+        breadcrumb = get_breadcrumb(category)
+
+        # 渲染页面
+        context = {
+            'categories': categories,
+            'breadcrumb': breadcrumb,
+            'sku': sku,
+            'spu': spu,
+            'spec_qs': spu_spec_qs,
+            'category': category
+
+        }
+        return render(request, 'detail.html', context)
